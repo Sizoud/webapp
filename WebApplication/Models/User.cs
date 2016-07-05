492a31bcb4b34b2e5fb617a3d4eaf70832cb645c
Admin <r.antonenko2017@yandex.ru>
2016-07-05 21:04:04 +0300

    Добавлена модель User. Добавлено подключение к БД

---
+++ WebApplication/Models/User.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        
+        [Required]
+        [Display(Name = "Логин")]
+        [MaxLength(50, ErrorMessage = "Превышена максимальная длина записи")]
+        public string Login { get; set; }
+        
+        [Required]
+        [Display(Name = "Пароль")]
+        [MaxLength(50, ErrorMessage = "Превышена максимальная длина записи")]
+        public string Password { get; set; }
+        
+    }
+}
